b9494295e26c9ad4b11445de7f07f8c7f23b244c
Brad Qiu <bradqiu1982@163.com>
2020-11-20 08:59:17 +0800

    update to ii-vi label

--- SkyEye/Models/DBUtility.cs
+++ SkyEye/Models/DBUtility.cs
@@ -265,6 +265,8 @@ namespace SkyEye.Models
                 command = conn.CreateCommand();
                 command.CommandText = sql;
                 command.CommandText = "SET ARITHABORT ON;" + command.CommandText;
+                command.CommandTimeout = 120;
+
                 if (parameters != null)
                 {
                     foreach (var param in parameters)
@@ -437,7 +439,8 @@ namespace SkyEye.Models
                 command = conn.CreateCommand();
                 command.CommandText = sql;
                 command.CommandText = "SET ARITHABORT ON;" + command.CommandText;
-                command.CommandTimeout = 45;
+                command.CommandTimeout = 120;
+
                 if (parameters != null)
                 {
                     foreach (var param in parameters)
